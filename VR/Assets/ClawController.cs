f5e29c188fe5ef6fe42f70867916ce95dde5683f
Jodelahithit <evancrasbeek@xs4all.nl>
2020-11-02 17:07:37 +0100

    Added SteamVR and test scene.

---
+++ VR/Assets/ClawController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ClawController : MonoBehaviour {
+    private List<GameObject> pivots = new List<GameObject>();
+    public LinearMapping linearMapping;
+
+    void Start() {
+        for (int i = 0; i < transform.childCount; i++) {
+            GameObject child = transform.GetChild(i).gameObject;
+            if (child.name.StartsWith("ClawPivot")) {
+                pivots.Add(child);
+                Vector3 rot = child.transform.localRotation.eulerAngles;
+                rot.z = 40;
+                child.transform.localRotation = Quaternion.Euler(rot);
+            }
+        }
+    }
+
+    bool a = false;
+    private float value = 0.0f;
+    void Update() {
+        value = linearMapping.value * 50;
+        //if (a) {
+        //    value += 20 * Time.deltaTime;
+        //    if(value > 50.0f) a = false;
+        //} else {
+        //    value -= 20 * Time.deltaTime;
+        //    if (value < 0.0f) a = true;
+        //}
+
+        foreach (var pivot in pivots) {
+            Vector3 rot = pivot.transform.localRotation.eulerAngles;
+            rot.z = value;
+            pivot.transform.localRotation = Quaternion.Euler(rot);
+        }
+    }
+}
---
+++ VR/Assets/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TestScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        foreach (var a in inputDevices) {
+            print(a.name);
+        }
+
+        XRController controller = GetComponent<XRController>();
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool menuButton)) {
+            print("Menu button: " + menuButton);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
