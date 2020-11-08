740d25e4ba276445d8400447a20ad407a611a648
Jodelahithit <evancrasbeek@xs4all.nl>
2020-11-08 19:47:49 +0100

    Added SteamVR; Added sliders; Added control panel; Added moveable ball.

---
+++ VR/Assets/LinearMappingObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class LinearMappingObjectController : MonoBehaviour {
+    public LinearMapping mapVertical;
+    public LinearMapping mapHorizontal;
+
+    private Vector3 velocity = new Vector3();
+    void FixedUpdate() {
+        velocity *= 0.95f;
+        velocity -= new Vector3((mapHorizontal.value - 0.5f) * 0.003f, 0, (mapVertical.value - 0.5f) * 0.003f);
+        transform.position += velocity;
+        if (transform.position.z < 3.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 3.0f);
+            velocity.z = 0.0f;
+        }
+    }
+}
