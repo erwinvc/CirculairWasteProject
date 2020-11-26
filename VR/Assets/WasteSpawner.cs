d53ce22ee9a3a08c0ab38ee73f770fb5deec9676
Jodelahithit <evancrasbeek@xs4all.nl>
2020-11-26 22:37:53 +0100

    Added colliders; Further scene development.

--- VR/Assets/WasteSpawner.cs
+++ VR/Assets/WasteSpawner.cs
@@ -2,24 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WasteSpawner : MonoBehaviour
-{
+public class WasteSpawner : MonoBehaviour {
     public List<GameObject> prefabs;
 
     bool started = true;
-    int iterations = 1000;
-    void FixedUpdate()
-    {
-        if(iterations > 0) {
+    int iterations = 100;
+    void FixedUpdate() {
+        if (iterations > 0) {
             iterations--;
+            for (int i = 0; i < 10; i++) {
 
-            Vector3 position = transform.position;
-            Vector3 size = transform.localScale;
+                Vector3 position = transform.position;
+                Vector3 size = transform.localScale;
 
-            Vector3 minPos = position - size * 0.5f;
-            Vector3 maxPos = position + size * 0.5f;
-            Instantiate(prefabs[Random.Range(0, prefabs.Count)],
-                new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), Random.Range(minPos.z, maxPos.z)), Quaternion.identity);
+                Vector3 minPos = position - size * 0.5f;
+                Vector3 maxPos = position + size * 0.5f;
+                Instantiate(prefabs[Random.Range(0, prefabs.Count)],
+                    new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y),
+                        Random.Range(minPos.z, maxPos.z)), Quaternion.identity);
+            }
         }
     }
 }
