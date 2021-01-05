69670da8535c4c04b3446d98d859dc700f66fdba
Jodelahithit <evancrasbeek@xs4all.nl>
2021-01-05 22:52:59 +0100

    Added automatic first scene loading; Minor scene improvements.

---
+++ VR/Assets/Magnet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+        switch (other.tag) {
+            case "WasteMetal":
+            case "WasteGlass":
+            case "WastePlastic":
+            case "WastePaper": MagnetGameObject(other.gameObject); break;
+        }
+    }
+
+    private void MagnetGameObject(GameObject obj) {
+        obj.GetComponent<Rigidbody>().AddForce(Vector3.up);
+    }
+}
--- VR/Assets/UpgradeManager.cs
+++ VR/Assets/UpgradeManager.cs
@@ -7,6 +7,7 @@ public class UpgradeManager : MonoBehaviour {
     private void Start() {
         DontDestroyOnLoad(this);
     }
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             SceneManager.LoadScene("Factory U1");
